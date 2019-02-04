b53f36d01beb4e2ecd8ccc11d49616f494f4cf8e
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-02-04 15:29:34 -0500

    Move icons

--- L5RTool/L5RUI/Converters/ElementTypeToImageConverter.cs
+++ L5RTool/L5RUI/Converters/ElementTypeToImageConverter.cs
@@ -8,7 +8,7 @@ namespace L5RUI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return "pack://application:,,,/L5RUI;component/Icons/" + value.ToString() + ".png";
+            return "pack://application:,,,/L5RUI;component/Icons/Elements/" + value.ToString() + ".png";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
