05f63c3754be33ea5a7bb8550de3d3738538b1a8
Pierre Olivier LePage <lepage.pierreolivier@gmail.com>
2019-02-05 10:46:25 -0500

    Prep for tab control

---
+++ L5RTool/L5RUI/Templates/ElementTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using NPC;
+using NPC.Model;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace L5RUI.Templates
+{
+    class ElementTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate DemeanorTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var element = item as IElement;
+            if (element != null)
+            {
+                switch (element.Type)
+                {
+                    case ElementType.Demeanor:
+                        return DemeanorTemplate;
+                    default:
+                        break;
+                }
+            }
+
+            return DefaultTemplate;
+        }
+    }
+}
